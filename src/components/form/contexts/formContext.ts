95db302779e3ede9c29019b087b6d24198f41de0
lailixiaoyiyao <yiyao_heihong@sina.cn>
2024-04-25 17:29:34 +0800

    a

--- src/components/form/contexts/formContext.ts
+++ src/components/form/contexts/formContext.ts
@@ -1,9 +1,6 @@
 import {createContext} from 'react';
+import Store from '../utils/store';
 
-const store = {
-    
-};
-
-const FormContext = createContext(store);
+const FormContext = createContext({} as Store);
 
 export default FormContext;
